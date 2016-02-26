83bd22616a70cf05df8400ec45fa6cc0fa3c9900
Stephen Kapp <mort666@virus.org>
2016-02-26 00:42:13 +0000

    Add Get All KB items

--- lib/qualys_api/knowledge.rb
+++ lib/qualys_api/knowledge.rb
@@ -1,6 +1,32 @@
 module Qualys
   class KnowledgeBase < Qualys::Base
 
+    def get_all
+      uri = '/api/2.0/fo/knowledge_base/vuln/'
+      params = {
+        action: 'list',
+        details: 'All'
+      }
+
+      response = get(uri, params)
+
+      unless response.body['KNOWLEDGE_BASE_VULN_LIST_OUTPUT']['RESPONSE'].has_key? 'VULN_LIST'
+        return []
+      end
+
+      kb = response.body['KNOWLEDGE_BASE_VULN_LIST_OUTPUT']['RESPONSE']['VULN_LIST']['VULN']
+
+      if kb.class == Hash
+        return [Qualys::Knowledge.new(kb)]
+      elsif kb.class == Array
+        kb.map! do |qid|
+          Qualys::Knowledge.new(qid)
+        end
+      else
+        return []
+      end
+    end
+
     def get_qid(qids)
       raise ArgumentError if qids.nil?
 
